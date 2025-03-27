bb97378d6bc3ae917fbd9a2e6f29ed4fb214045d
曾擎禹 <zengqingyu@cengqingyudeMacBook-Pro.local>
2025-03-28 00:50:54 +0800

    delete useless file

--- ros2_ws/src/stl_visualizer/stl_visualizer/stl_publisher.py
+++ ros2_ws/src/stl_visualizer/stl_visualizer/stl_publisher.py
@@ -10,10 +10,9 @@ class STLVisualizer(Node):
     def __init__(self):
         super().__init__('stl_visualizer')
         
-        # 静态TF广播器
         self.static_broadcaster = StaticTransformBroadcaster(self)
         
-        # 设置静态TF参数（根据SDF中的初始pose）
+        # define the transform between the map and the stl file
         static_transform = TransformStamped()
         static_transform.header.stamp = self.get_clock().now().to_msg()
         static_transform.header.frame_id = "map"
@@ -29,39 +28,30 @@ class STLVisualizer(Node):
         static_transform.transform.rotation.w = 0.0
     
   
-        
-        # 发布静态TF
         self.static_broadcaster.sendTransform(static_transform)
 
-        # 加载STL文件
         self.mesh = mesh.Mesh.from_file('/input/models/RMUL2024_world/meshes/RMUL_2024.stl')
         
-        # 创建发布者
         self.marker_pub = self.create_publisher(Marker, '/stl_marker', 10)
         
-        # TF监听器
         self.tf_buffer = Buffer()
         self.tf_listener = TransformListener(self.tf_buffer, self)
         
-        # 定时器
         self.timer = self.create_timer(1.0, self.publish_marker)
 
     def publish_marker(self):
         try:
-            # 获取map到link_3的变换
             transform = self.tf_buffer.lookup_transform(
                 'map',
                 'link_3',
                 rclpy.time.Time())
             
-            # 创建Marker消息
             marker = Marker()
             marker.header.frame_id = "map"
             marker.header.stamp = self.get_clock().now().to_msg()
             marker.type = Marker.MESH_RESOURCE
             marker.mesh_resource = "http://localhost:8000/RMUL_2024.stl"
             
-            # 设置位姿和缩放
             marker.pose.position.x = transform.transform.translation.x
             marker.pose.position.y = transform.transform.translation.y
             marker.pose.position.z = transform.transform.translation.z
@@ -70,7 +60,6 @@ class STLVisualizer(Node):
             marker.scale.y = 0.001
             marker.scale.z = 0.001
             
-            # 设置颜色
             marker.color.r = 0.7
             marker.color.g = 0.7
             marker.color.b = 0.7
@@ -79,7 +68,7 @@ class STLVisualizer(Node):
             self.marker_pub.publish(marker)
             
         except Exception as e:
-            self.get_logger().warn(f'TF转换异常: {str(e)}')
+            self.get_logger().warn(f'TF error: {str(e)}')
 
 def main(args=None):
     rclpy.init(args=args)
